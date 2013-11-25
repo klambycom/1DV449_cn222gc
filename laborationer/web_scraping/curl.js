20623e9084f79d64b5218012183725926dc083a5
klambycom <christian@klamby.com>
2013-11-25 03:52:10 +0100

    Curling tests

--- laborationer/web_scraping/curl.js
+++ laborationer/web_scraping/curl.js
@@ -1,14 +1,34 @@
 /*jslint node: true */
 'use strict';
 
-var curl = require('curling').connect();
+var curl = require('curling').connect({
+	location: null,
+	'cookie-jar': 'cookie.txt',
+	cookie: 'cookie.txt'
+});
 
+/*
 var options = {
 	header: "Accept: text/html",
-	location: null
+	location: null,
+	'cookie-jar': 'cookie.txt',
+	cookie: 'cookie.txt'
 };
 
-curl.get('http://www.google.com', options, function (err, result) {
+curl.get('http://vhost3.lnu.se:20080/~1dv449/scrape', options, function (err, result) {
+	console.log(result.payload);
+	console.log(result.stats);
+});
+*/
+
+var options = {
+	data: ['username=admin', 'password=admin']
+};
+
+curl.post('http://vhost3.lnu.se:20080/~1dv449/scrape/check.php', options, function (err, result) {
+	if (err) {
+		console.log(err);
+	}
 	console.log(result.payload);
 	console.log(result.stats);
 });
